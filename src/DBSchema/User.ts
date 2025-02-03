020e986d47a2e06ea75a6fcc939773206ce497d9
Adrien <adrienribeiro999@gmail.com>
2025-02-03 15:24:21 +0100

    Save infernale

--- src/DBSchema/User.ts
+++ src/DBSchema/User.ts
@@ -1,15 +1,20 @@
 import mongoose, { Document, Schema } from "mongoose"
 
+enum Role {
+    Admin = "Admin",
+    User = "User",
+}
+
 export interface UserI extends Document {
     name: string;
     password: string;
-    role: string;
+    userRole: Role;
 }
 
 const UserSchema = new Schema({
     name: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, required: true }
+    role: { type: Role, required: true }
 })
 
 export default mongoose.model<UserI>('User', UserSchema);
