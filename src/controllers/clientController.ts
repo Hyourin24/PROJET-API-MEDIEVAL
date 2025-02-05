950968701deee2cffd601444cad72b31e5e36ef2
Melissa <melissa.pegas@hotmail.fr>
2025-02-05 12:05:44 +0100

    ajouté route modifier actif

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -75,12 +75,38 @@ export async function modifyClient(req:Request, res: Response){
             return;
         }
 
-
-        
-        //réponse réussie
         res.status(200).json({message:'Client mis à jour avec succès', data: updatedClientData});
     } catch(err:any)  {
-        //Gestion des erreurs
+
+        res.status(500).json({message:'Erreur interne', error:err.message})
+    }
+}
+
+export async function modifyClientActif(req:Request, res: Response){
+    try{
+        const {id}= req.params; 
+        const {nom, adresse, email, téléphone, actif} = req.body;
+          if(!id){
+            res.status(400).json({message:'ID requis'});
+            return
+          }
+      
+        const updatedActif = await Clients.findByIdAndUpdate(
+            id, //ID de l'utilisateur à mettre à jour
+            {completed:true}, 
+            {new:true, runValidators:true}
+        );
+
+        const updatedActifData = { actif };
+        
+        if(!updatedActif){
+            res.status(404).json({message:'Actif requis'});
+            return
+        };
+
+        res.status(200).json({message:'Actif mis à jour avec succès', data: updatedActifData});
+    } catch(err:any)  {
+
         res.status(500).json({message:'Erreur interne', error:err.message})
     }
 }
--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createClient, modifyClient } from "../controllers/clientController";
+import { createClient, modifyClient, modifyClientActif } from "../controllers/clientController";
 import { verifyTokenMiddleware } from "../verifyTokenMiddleware";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post('/create', createClient);
 router.put('/modify/clients/:id', verifyTokenMiddleware, modifyClient);
+router.put('/modify/actif/:id', verifyTokenMiddleware, modifyClientActif);
 
 
 export default router;
