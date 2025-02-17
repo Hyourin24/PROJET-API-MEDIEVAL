618a0b447f54242a23511752d0dcc402fc5b2751
Adrien <adrienribeiro999@gmail.com>
2025-02-17 14:53:57 +0100

    save commande annulée

--- src/controllers/DashboardController.ts
+++ src/controllers/DashboardController.ts
@@ -5,35 +5,14 @@ import ProduitsSchema,  {ProduitsI} from '../DBSchema/ProduitsSchema';
 
 export async function getRevenue(req: Request, res: Response): Promise<void> {
     try {
-        const commandes = await Commandes.find();
-        const commandeId = req.params.id;
-        const commande = await Commandes.findById(commandeId);
-        
-        if (!commande) {
-            res.status(404).json({ message: "Commande introuvable" });
-            return;
-        }
-    
-            const { status } = commande; 
-            let actualStatus: string = status
-            let newStatus: string = "";
-    
-            if (actualStatus == "En attente" || actualStatus === "Expédiée" || actualStatus === "Livrée") {
-                newStatus = "Annulée";
-            }
-
-        if (actualStatus == "En attente" || actualStatus === "Expédiée" || actualStatus === "Livrée") {
-            newStatus = "Annulée";
-        }
+        const commandes = await Commandes.find({ status: { $ne: "Annulée" } }).exec();
         if (!commandes) {
             res.status(404).json({ message: "Aucune commande" });
             return;
         }
         const revenuTotal = commandes.reduce((acc: number, commande: any) => acc + (commande.montantTotal || 0), 0);
 
-        res.status(200).json({message: "Total des revenus de ce mois-ci", revenuTotal
-});
-
+        res.status(200).json({message: "Total des revenus de ce mois-ci", revenuTotal});
     } catch (err: any) {
         res.status(500).json({ message: "Erreur interne", error: err.message });
     }
